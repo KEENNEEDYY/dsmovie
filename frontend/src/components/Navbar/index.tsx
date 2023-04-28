2c13ae60164d52b575dc421d796f3fe7983df958
KENNEDY LEOCADIO <81121745+KEENNEEDYY@users.noreply.github.com>
2023-04-28 19:46:50 -0300

    Update navbar index.tsx

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar(){
               <a href="https://github.com/KEENNEEDYY">
                 <div className="dsmovie-contact-container">
                   <GithubIcon/>
-                  <p className="dsmovie-contact-link">/devsuperior</p>
+                  <p className="dsmovie-contact-link">/KENNEDY</p>
                 </div>
               </a>
             </div>
